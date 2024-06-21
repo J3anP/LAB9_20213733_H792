fe922db24d5bbf52dadb0e2e6455bf3e196608b1
J3anP <a20213733@pucp.edu.pe>
2024-06-20 19:15:17 -0500

    first commit

---
+++ src/main/java/org/example/stfgames/StfgamesApplication.java
@@ -0,0 +1,13 @@
+package org.example.stfgames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StfgamesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StfgamesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/stfgames/StfgamesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.stfgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StfgamesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
